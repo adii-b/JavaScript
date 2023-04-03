93c94dd280432cd16d193b39cb93e514451dbc17
Aditya Bharadwaj <adityabharadwaj273@gmail.com>
2023-04-03 22:18:33 +0530

    New files added

---
+++ 07.js
@@ -0,0 +1,3 @@
+let a = [1, "pw skills", "pw skills", "pw skills", 3, 4];
+console.log(a.indexOf("pw skills")); // first index of "pw skills"
+console.log(a.lastIndexOf("pw skills")); // last index of "pw skills"
