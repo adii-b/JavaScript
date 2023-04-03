ef91e0b333aa46b7717fd34352965157729fa718
Aditya Bharadwaj <adityabharadwaj273@gmail.com>
2023-04-04 00:43:09 +0530

    added new files

---
+++ 11.js
@@ -0,0 +1,9 @@
+const a = new Date();
+console.log(a.getFullYear()); // Year
+console.log(a.getMonth()); // Month number
+console.log(a.toString()); // Date
+console.log(a.getDay()); // Day number
+console.log(a.getHours()); // Hour number
+console.log(a.getMinutes()); // Minute number
+
+console.log(a.getTime() / 1000); // Number of seconds elpased since 1st Jan 1970
