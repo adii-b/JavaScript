a65f521b560727038dfe1d81b601dc52ec8afc96
Aditya Bharadwaj <adityabharadwaj273@gmail.com>
2023-04-03 23:49:22 +0530

    updated files

--- 08.js
+++ 08.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 let a = "  Aditya   ";
 console.log(a);
 console.log(a.trim());
-=======
-let a = "  Aditya   ";
-console.log(a);
-console.log(a.trim());
->>>>>>> 1c71a6e22fdff5996b8e41bcd21cf8d752be0d54
